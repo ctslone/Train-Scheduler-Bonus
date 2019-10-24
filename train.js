e993a3ae3e60bcd66aea19d943aa0c53e1c87d93
ctslone <ctslone27@gmail.com>
2019-10-24 15:24:06 -0400

    adding authenitication to the train app. auth is initialized but need to deploy to pages to test if working. also need to prevent a user from skipping the login all together

--- train.js
+++ train.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+
     // adding FB to the app
     var firebaseConfig = {
         apiKey: "AIzaSyCsnN2fUQv34EoUy56xJJLir0ISDRF27Q8",
@@ -25,13 +25,13 @@ $(document).ready(function () {
         form.preventDefault();
         // re-declaring the variables to the user input
         trainName = $("#train-name-input").val().trim();
-            // console.log(trainName);
+        // console.log(trainName);
         destination = $("#destination-input").val().trim();
-            // console.log(destination);
+        // console.log(destination);
         frequency = $("#frequency-input").val().trim();
-            // console.log(frequency);
+        // console.log(frequency);
         firstTime = $("#first-time-input").val().trim();
-            // console.log(firstTime);
+        // console.log(firstTime);
         // console.log("These are the variables:" + trainName, destination, frequency, nextArrival, minsAway)
 
         // creating new child for the variables in firebase
@@ -47,57 +47,61 @@ $(document).ready(function () {
         $("#train-name-input, #destination-input, #frequency-input, #first-time-input").val('')
     })
 
-        // listener for adding a child to the DB
-        database.ref().on("child_added", function(snapshot) {
-            // creating a varible that holds a snapshot of the data at that moment (hence snapshot)
-            var sv = snapshot.val();
+    // listener for adding a child to the DB
+    database.ref().on("child_added", function (snapshot) {
+        // creating a varible that holds a snapshot of the data at that moment (hence snapshot)
+        var sv = snapshot.val();
+        // current time
+        var currentTime = moment();
+        // creating a reference in the past so that JS has a previous time to reference
+        var firstTrain = moment(sv.firstTime, "HH:mm").subtract(1, "years");
+        // console.log("First Train: " + firstTrain)
+        // the difference in time in minutes between the first train one year ago and the current time
+        var diffTime = currentTime.diff(moment(firstTrain), "minutes");
+        // console.log("Difference in time is: " + diffTime);
+        // the remainder of time leftover after the difference in time is divided by the user input of frequency
+        var remainTime = diffTime % sv.frequency;
+        // console.log("Remainder minutes: " + remainTime);
+        // calcualting time till next train in minutes
+        var timeTillNext = sv.frequency - remainTime;
+        // console.log("Time till next train: " + timeTillNext);
+        // calculating the next arrivial time in minutes from now
+        var nextArrival = moment().add(timeTillNext, "minutes")
 
-            // checking
-            // console.log("snapshot: " + sv.trainName);
-            // console.log("snapshot: " + sv.destination);
-            // console.log("snapshot: " + sv.frequency);
-            // console.log("snapshot: " + sv.firstTime);
-            // console.log("snapshot: " + sv.dateAdded);
+        var nameTd = $("<td>").text(sv.trainName);
+        var destinationTd = $("<td>").text(sv.destination);
+        var frequencyTd = $("<td>").text(sv.frequency);
+        // var nextArrivalTd = $("<td>").text(timeTillNext);
+        // converting minutes into h:mm format and adding ap/pm (a)
+        var minutesAwayTd = $("<td>").text(moment(nextArrival).format("h:mm a"));
 
-            // current time
-            var currentTime = moment();
-            // creating a reference in the past so that JS has a previous time to reference
-            var firstTrain = moment(sv.firstTime, "HH:mm").subtract(1, "years");
-                console.log("First Train: "+firstTrain)
-            // the difference in time in minutes between the first train one year ago and the current time
-            var diffTime = currentTime.diff(moment(firstTrain), "minutes");
-                console.log("Difference in time is: " + diffTime);
-            // the remainder of time leftover after the difference in time is divided by the user input of frequency
-            var remainTime = diffTime % sv.frequency;
-                console.log("Remainder minutes: "+remainTime);
-            // calcualting time till next train in minutes
-            var timeTillNext = sv.frequency - remainTime;
-                console.log("Time till next train: "+timeTillNext);
-            // calculating the next arrivial time in minutes from now
-            var nextArrival = moment().add(timeTillNext, "minutes")
+        // make it so moment JS updates every minute for next arrivial and minutes away
+        // need to use setInterval for this
+        // make all of the desired varibles (nextarrival, timetill) in a function and have that funcction be called every 60 seconds
 
-            var nameTd = $("<td>").text(sv.trainName);
-            var destinationTd = $("<td>").text(sv.destination);
-            var frequencyTd = $("<td>").text(sv.frequency);
+        function updateTime() {
             var nextArrivalTd = $("<td>").text(timeTillNext);
-            // converting minutes into h:mm format and adding ap/pm (a)
-            var minutesAwayTd = $("<td>").text(moment(nextArrival).format("h:mm a"));
+        }
+        
+        updateTime();
+        setInterval(function () {
+            updateTime();
+        }, 60000);
 
-            // appending th variables to the row and then prepending the row to the table
-            var tR = $("<tr>");
-            tR.append(nameTd, destinationTd, frequencyTd, minutesAwayTd, nextArrivalTd)
-            $("#train-table").prepend(tR);
-            // console.log("added")
-            
-            // playing a sweet train sound when the user adds a train!
-            trainHorn();
-        })
+        // appending th variables to the row and then prepending the row to the table
+        var tR = $("<tr>");
+        tR.append(nameTd, destinationTd, frequencyTd, minutesAwayTd)
+        $("#train-table").prepend(tR);
+        // console.log("added")
 
-// OTHER FUNCTIONS
+        // playing a sweet train sound when the user adds a train!
+        trainHorn();
+    })
 
-function trainHorn(horn) {
-    var horn = document.createElement("audio");
-    horn.src = "media/horn.mp3"
-    horn.play();
-}
-})
+    // OTHER FUNCTIONS
+
+    function trainHorn(horn) {
+        var horn = document.createElement("audio");
+        horn.src = "media/horn.mp3"
+        horn.play();
+    }
